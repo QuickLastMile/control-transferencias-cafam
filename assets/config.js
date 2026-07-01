21da933c70ffe0648c42d4ef4694af1765def47f
Quick LastMile <quick.helpai2026@gmail.com>
2026-06-30 20:03:17 -0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -3,6 +3,6 @@
  * El TOKEN debe ser EXACTAMENTE el mismo que pusiste en CONFIG.TOKEN de Code.gs.
  */
 window.QK_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzI7ZIjtV66Dt99GkFjw6gAZk62yb5zOINjiWflsdaooULam5Jsix-KljQinAa8_oP-/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzF7gL6YDS8PlBt6zz-Sq9UJkzq6St2TSumF1FKCOfgWu_bt7XydmZ38Tdtl9sbAhqCqA/exec",
   TOKEN: "QK-TRANSFERENCIAS-1234567890",
 };
